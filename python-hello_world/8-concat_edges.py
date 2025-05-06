193543e2a86f718f44eb66b6188b00a3d8b8ecef
MaximoDilacio <developermaximo1@gmail.com>
2025-05-06 18:04:58 +0000

    Ejercicio 6

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\language that combines remarkable power with very clear syntax"
-str1 = (str[39:-63]) + " " + "with python"
-print(str1)
+print((str[39:-63]) + " " + "with python")
