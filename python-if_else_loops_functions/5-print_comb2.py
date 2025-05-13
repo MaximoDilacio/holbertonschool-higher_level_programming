71f3d991b54a19b8a15b545aa89a8e7e1059d076
MaximoDilacio <developermaximo1@gmail.com>
2025-05-13 13:47:20 +0000

    File fix 5

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -4,5 +4,5 @@ while (i <= 99):
     if (i == 99):
         print("{:02d}\n".format(i), end = "")
     else:
-        print("{:02d}, ".format(i), end = "")
+        print("{:02d},".format(i), end = "")
     i += 1
