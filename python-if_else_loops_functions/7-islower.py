c600adb15b2e3cadf922262d12b3b3d499f9b41e
MaximoDilacio <developermaximo1@gmail.com>
2025-05-13 14:15:35 +0000

    File 7

--- python-if_else_loops_functions/7-islower.py
+++ python-if_else_loops_functions/7-islower.py
@@ -3,5 +3,5 @@ def islower(c):
     numero = ord(c)
     if (numero >= 65 and numero < 90):
         print("{} is upper".format(chr(numero)))
-    elif (numero >= 97 and numero <= 122):
+    elif (numero >= 96 and numero <= 122):
         print("{} is lower".format(chr(numero)))
